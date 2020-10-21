f24ae8c0b5f1baab21ad1a741b1f8e61160071f1
katherinetvl <63134770+katherinetvl@users.noreply.github.com>
2020-10-21 18:15:01 -0400

    Update index.js
    Made dramatic changes. Untested.

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const twilio = require('twilio');
+const e = require('express');
 
 //const accountSid = "";
 //const authToken = "";
 
-var enterAccountSid = process.argv[2];
-var enterAuthToken = process.argv[3];
+let enterAccountSid = process.argv[2];
+let enterAuthToken = process.argv[3];
 
 if (enterAccountSid && enterAuthToken)
 {
@@ -17,25 +18,87 @@ if (enterAccountSid && enterAuthToken)
 
     const MessagingResponse = require("twilio").twiml.MessagingResponse;
 
-    let lastMessage = "";
+    let idArr = [];
+    let noGamesArr = [];
 
     let app = express();
 
     app.use(bodyParser.urlencoded({extended:false}));
+ 
+    app.post('/sms', function (request, response)
+    {
+        const twiml = new MessagingResponse();
+        console.log(request.body); 
+        var idUser = request.body.From;
+        var idIndex = idArr.indexOf(idUser);
+        if(idIndex < 0)
+        {
+            idArr.push(idUser);
+            noGamesArr.push(0); 
+        }
+        twiml.message("Hello! Would you like to start a new game? (yes or no)");
+        
+        switch(request.body.Body.toLowerCase())
+        {
+            case 'yes' :
+            twiml.message("Guess a number between 1 and 1000");
+            console.log(request.body); 
+
+            let randomNumber = Math.floor(Math.random() * 11);
+            let userGuess = request.body.Body;
+            if (userGuess == randomNumber)
+            {
+                console.log(request.body); 
+                twiml.message("Way to go! You win! The number was " + randomNumber);
+                var gamesPlayed = noGamesArr[idIndex];
+                noGamesarr[idIndex] = gamesPlayed++; 
+                twiml.message("Your number of games played: " + noGamesArr[gamesPlayed]);
+            } 
+            else if (userGuess < randomNumber)
+            {
+                var gamesPlayed = noGamesArr[idIndex];
+                noGamesarr[idIndex] = gamesPlayed++; 
+                twiml.message("Sorry, your guess is too low.");
+                twiml.message("Your number of games played: " + noGamesArr[gamesPlayed]);
+            }
+            else
+            {
+                var gamesPlayed = noGamesArr[idIndex];
+                noGamesarr[idIndex] = gamesPlayed++; 
+                twiml.message("Sorry, your guess is too high!");
+                twiml.message("Your number of games played: " + noGamesArr[gamesPlayed]);
+            }
+            break;
+
+            case 'no': 
+            twiml.message("Message back when you want to play!");
+            break;
+
+            default:
+            twiml.messsage("Invalid response.");
+            break;
+        }  
+        response.writeHead(200,{'Content-Type' : 'text/xml'});
+        response.end(twiml.toString());
+    });
 
     // when incoming messages happen 
+    /*
     app.post('/sms', function (request, response)
     {
         const twiml = new MessagingResponse();
 
-        switch(request.body.Body.toLowerCase())
+        // start game 
+        let randomNumber = rand(0,1000);
+        let n = request.body.Body;
+        switch(n)
         {
             case 'last':
                 twiml.message(lastMessage);
                 break;
             default:
                 lastMessage = request.body.Body;
-                twiml.message("You have registered this message as the last message. Enter 'last' to see the previous message at any time.");
+                twiml.message("Take a guess!");
         }
 
         response.writeHead(200,{'Content-Type' : 'text/xml'});
@@ -43,7 +106,9 @@ if (enterAccountSid && enterAuthToken)
         response.end(twiml.toString());
     });
 
+    */
     app.listen(8000);
+
 }
 else 
 {
